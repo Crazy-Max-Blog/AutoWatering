630d65da5dd06fcbcf4256c0e26d552e724e035c
Crazy-Max-2011 <Crazy-Max-2011@yandex.ru>
2025-03-25 21:40:50 +0400

    Add support sets water time sets in Settings

--- src/sensor/water.h
+++ src/sensor/water.h
@@ -15,7 +15,7 @@ namespace sensor {
 
     void water(byte id) {
         relay.setPin(id, 1);
-        delay(WATER_TIME);
+        delay((int)db::db[id == 0 ? kk::water_time_1 : kk::water_time_2] * 1000);
         relay.setPin(id, 0);
     }
 }
